01fea6bdd3e30cc9ba3b4110efd2914628a2cd92
Romain Chaumais <romain@chaumais.com>
2019-05-22 00:09:05 +0200

    Add SYnc Gard in the Easy Firestore Plugin

--- src/store/mirror-exc-gcs-to-gcs-confs/index.js
+++ src/store/mirror-exc-gcs-to-gcs-confs/index.js
@@ -4,6 +4,9 @@ const mirrorExcGcsToGcsConfs = {
   moduleName: "mirrorExcGcsToGcsConfs",
   statePropName: "data",
   namespaced: true, // automatically added
+  sync: {
+    guard: ["created_at", "updated_at"] // array of keys
+  },
 
   // this object is your store module (will be added as '/myModule')
   // you can also add state/getters/mutations/actions
--- src/store/vm-launcher-confs/index.js
+++ src/store/vm-launcher-confs/index.js
@@ -4,6 +4,10 @@ const vmLauncherConfs = {
   moduleName: "vmLauncherConfs",
   statePropName: "data",
   namespaced: true, // automatically added
+  sync: {
+    guard: ["created_at", "updated_at"] // array of keys
+  },
+
 
   // this object is your store module (will be added as '/myModule')
   // you can also add state/getters/mutations/actions
