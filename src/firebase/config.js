5766e8fe406f0d202fda899eb0a3eaa29be1cbc2
Romain Chaumais <romain.chaumais@fashiondata.io>
2019-11-16 14:27:14 +0100

    Delete Config Files

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,10 +1,10 @@
 const config = {
-  apiKey: "AIzaSyCJ6bMFiWaUWYs5lc9Y1Pz6VJiq_v1_VRU",
-  authDomain: "fd-jarvis-datalake.firebaseapp.com",
-  databaseURL: "https://fd-jarvis-datalake.firebaseio.com",
-  projectId: "fd-jarvis-datalake",
-  storageBucket: "fd-jarvis-datalake.appspot.com",
-  messagingSenderId: "581360803013"
+	apiKey: "AIzaSyCJ6bMFiWaUWYs5lc9Y1Pz6VJiq_v1_VRU",
+	authDomain: "fd-jarvis-datalake.firebaseapp.com",
+	databaseURL: "https://fd-jarvis-datalake.firebaseio.com",
+	projectId: "fd-jarvis-datalake",
+	storageBucket: "fd-jarvis-datalake.appspot.com",
+	messagingSenderId: "581360803013"
 };
 
 export default config;
