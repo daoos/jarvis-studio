ad4cd546a476ae4c16746977277e00d1e2505499
Eronne <erwann.letue@gmail.com>
2019-11-23 14:27:48 +0100

    refactor(AccountSelector): Refactored component

--- src/components/widgets/filters/AccountSelector.vue
+++ src/components/widgets/filters/AccountSelector.vue
@@ -3,16 +3,14 @@
 		<v-menu top offset-y>
 			<template v-slot:activator="{ on }">
 				<v-btn v-on="on" text>
-					<span class="body-2">
-						{{ accountFilterSelected.account_name }}
-					</span>
+					<span class="body-2">{{ accountFilterSelected.account_name }}</span>
 					<v-icon>arrow_drop_down</v-icon>
 				</v-btn>
 			</template>
 
 			<v-list>
 				<v-list-item v-for="account in accountArray" :key="account.id" @click="applyAccountFilter(account)">
-					<v-list-item-title v-text="account.account_name"></v-list-item-title>
+					<v-list-item-title v-text="account.account_name" />
 				</v-list-item>
 			</v-list>
 		</v-menu>
@@ -21,7 +19,7 @@
 
 <script>
 import { mapState } from "vuex";
-import store from "@/store/index";
+import store from "@/store";
 
 export default {
 	components: {},
@@ -37,38 +35,15 @@ export default {
 	},
 	computed: {
 		...mapState({
-			isAuthenticated: state => state.user.isAuthenticated,
 			user: state => state.user.user,
 			accountFilterSelected: state => state.filters.accountFilterSelected,
 			accounts: state => state.accounts.data
 		}),
 		accountArray() {
 			let accountArray = {};
-			// if (this.user.studioRoles == 5) {
-			//   accountArray = Object.values(this.accounts);
-			//   accountArray.push({ account_name: "All Accounts", id: "000000" });
-			// } else if (this.user.accounts.length > 0) {
-			//   for (const account of this.user.accounts) {
-			//     accountArray.push({ account_name: account, id: account });
-			//   }
-			// }
 			accountArray = Object.values(this.accounts);
 			accountArray.push({ account_name: "All Accounts", id: "000000" });
 			return accountArray;
-		},
-		//Set the selected account to the accounts attached to the user Custom Claim
-		accountFilterSelectedForUser() {
-			let accountFilterSelectedForUser = {};
-			if (this.user.studioRoles === 5) {
-				accountFilterSelectedForUser = this.accountFilterSelected;
-			}
-			if (this.user.accounts.length > 0) {
-				accountFilterSelectedForUser = {
-					account_name: this.user.accounts[0],
-					id: this.user.accounts[0]
-				};
-			}
-			return accountFilterSelectedForUser;
 		}
 	}
 };
