28f88de636b7b4137d8e25032328e8bc31b7d077
Romain Chaumais <romain.chaumais@fashiondata.io>
2019-11-30 12:22:03 +0100

    Add Tables to Storage Store Object

--- src/store/index.js
+++ src/store/index.js
@@ -40,6 +40,7 @@ import storageToTableConf from '@/store/storage-to-table-conf';
 import storageToTableRun from '@/store/storage-to-table-run';
 import tablesToTablesConf from '@/store/tables-to-tables-conf';
 import tablesToTablesRun from '@/store/tables-to-tables-run';
+import tablesToStorageConfs from './tables-to-storage-confs';
 
 const easyFirestores = VuexEasyFirestore(
 	[
@@ -70,6 +71,7 @@ const easyFirestores = VuexEasyFirestore(
 		storageToTableRun,
 		tablesToTablesConf,
 		tablesToTablesRun,
+		tablesToStorageConfs,
 		singleDoc
 	],
 	{
