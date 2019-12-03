08773e389dcbb7ed76c523119035a79b74dddc4d
Eronne <erwann.letue@gmail.com>
2019-12-03 15:49:24 +0100

    chore(gcs-to-gbq): Moved StorageToTable components

--- src/views/data-workflows/gcs-to-gbq/StorageToTableConf.vue
+++ src/views/data-workflows/gcs-to-gbq/StorageToTableConf.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<DataManagementHeader :tabsItems="tabsItems" :headerTitle="headerTitle"></DataManagementHeader>
+		<DataManagementHeader :tabsItems="tabsItems" :headerTitle="headerTitle" />
 		<StorageToTableConf />
 	</div>
 </template>
