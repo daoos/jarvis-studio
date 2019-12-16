ac678a4f94c36b85966ad1ec8ccd823b50077809
Romain Chaumais <romain.chaumais@fashiondata.io>
2019-12-16 13:50:39 +0100

    Finalization Storage To Tables configurations

--- src/views/data-workflows/storage-to-tables/configurations/ListingView.vue
+++ src/views/data-workflows/storage-to-tables/configurations/ListingView.vue
@@ -12,6 +12,9 @@
 					<span class="font-weight-medium">{{ id }}</span>
 				</router-link>
 			</template>
+			<template v-slot:source="{ item: { source } }">
+				{{ source.type === 'gcs' ? source.gcs_source_bucket : 'unknown' }}
+			</template>
 		</listing-component>
 	</div>
 </template>
@@ -31,7 +34,7 @@ export default {
 	data() {
 		return {
 			moduleName: 'storageToTablesConfs',
-			overriddenColumns: ['id']
+			overriddenColumns: ['id', 'source']
 		};
 	},
 	computed: {
@@ -57,6 +60,20 @@ export default {
 					align: 'left',
 					sortable: true,
 					value: 'id'
+				},
+				{ text: 'Source Type', align: 'left', sortable: true, value: 'source.type' },
+				{ text: 'Source Name', align: 'left', sortable: true, value: 'source' },
+				{
+					text: 'Destinations',
+					align: 'left',
+					sortable: true,
+					value: 'destinations.length'
+				},
+				{
+					text: 'Tables',
+					align: 'left',
+					sortable: true,
+					value: 'destinations[0].tables.length'
 				},
 				{
 					text: 'Status',
