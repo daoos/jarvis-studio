7e8b2ca71a2a611727f42a8524a4326ee458f656
Eronne <erwann.letue@gmail.com>
2020-02-23 18:06:42 +0100

    chore(functions): Create user folder in handlers

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const listUsersHandler = require('./handlers/list-users');
-const createUserHandler = require('./handlers/create-user');
-const updateUserHandler = require('./handlers/update-user');
-const deleteUserHandler = require('./handlers/delete-user');
+const listUsersHandler = require('./handlers/user/list-users');
+const createUserHandler = require('./handlers/user/create-user');
+const updateUserHandler = require('./handlers/user/update-user');
+const deleteUserHandler = require('./handlers/user/delete-user');
+const insertNoteHandler = require('./handlers/notes/insert-note');
 
 admin.initializeApp();
 
@@ -12,3 +13,4 @@ exports.listUsers = functions.https.onCall((data, context) => listUsersHandler(d
 exports.createUser = functions.https.onCall((data, context) => createUserHandler(data, context));
 exports.updateUser = functions.https.onCall((data, context) => updateUserHandler(data, context));
 exports.deleteUser = functions.https.onCall((data, context) => deleteUserHandler(data, context));
+exports.insertNote = functions.https.onCall((data, context) => insertNoteHandler(data, context));
