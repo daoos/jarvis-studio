363449381679ef108d8b81ad33fb740cae2f6b1f
Eronne <erwann.letue@gmail.com>
2020-02-27 13:49:28 +0100

    chore(functions): Removed insertNote CF

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,14 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const listUsersHandler = require('./handlers/user/list-users');
-const createUserHandler = require('./handlers/user/create-user');
-const updateUserHandler = require('./handlers/user/update-user');
-const deleteUserHandler = require('./handlers/user/delete-user');
-
-const insertNoteHandler = require('./handlers/notes/insert-note');
+const listUsersHandler = require('./handlers/list-users');
+const createUserHandler = require('./handlers/create-user');
+const updateUserHandler = require('./handlers/update-user');
+const deleteUserHandler = require('./handlers/delete-user');
 
 admin.initializeApp();
 
-// User
 exports.listUsers = functions.https.onCall((data, context) => listUsersHandler(data, context));
 exports.createUser = functions.https.onCall((data, context) => createUserHandler(data, context));
 exports.updateUser = functions.https.onCall((data, context) => updateUserHandler(data, context));
 exports.deleteUser = functions.https.onCall((data, context) => deleteUserHandler(data, context));
-
-// Notes
-exports.insertNote = functions.https.onCall((data, context) => insertNoteHandler(data, context));
